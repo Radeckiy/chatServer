6ec6490e8b9d89755a5e42d4afd81c64e5645c90
Andrey <radeckiy98@mail.ru>
2019-09-25 20:27:50 +0500

    Был почищен WebSocketEventListener.

--- src/main/java/com/radeckiy/chat/listeners/WebSocketEventListener.java
+++ src/main/java/com/radeckiy/chat/listeners/WebSocketEventListener.java
@@ -2,9 +2,6 @@ package com.radeckiy.chat.listeners;
 
 import com.radeckiy.chat.models.Message;
 import com.radeckiy.chat.models.MessageType;
-import org.bson.types.ObjectId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
@@ -14,7 +11,8 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
 public class WebSocketEventListener {
-    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+    // Логгер
+    //private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
 
     private SimpMessageSendingOperations messagingTemplate;
 
@@ -23,9 +21,7 @@ public class WebSocketEventListener {
     }
 
     @EventListener
-    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
-        logger.info("Received a new web socket connection..");
-    }
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {}
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
